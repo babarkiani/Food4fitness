1dc19cbec90416b4c5a9e04649228f7b6d6c646f
unknown <rajasarmad21@gmail.com>
2019-04-09 04:59:11 -0700

    header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ElementRef,ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   isCollapsed = false;
   constructor() { }
-
+@ViewChild('mySidebar')side:ElementRef;
+@ViewChild('btt')main:ElementRef;
   ngOnInit() {
   }
+
+openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  // document.getElementById("main").style.marginLeft = "250px";
+  this.main.nativeElement.show=false;
+}
+
+closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  // document.getElementById("main").style.marginLeft= "0";
+  this.main.nativeElement.show=true;
+}
+
 
 }
